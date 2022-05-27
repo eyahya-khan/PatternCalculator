e833d8de3e3640341377dcb52141d3951b2f516d
eyahya-khan <eyahyakhan@gmail.com>
2022-05-27 20:07:21 +0200

    Add structure

--- Patterns.Calculator/Solution.cs
+++ Patterns.Calculator/Solution.cs
@@ -2,5 +2,44 @@
 {
   public class FluentCalculator
   {
+    public enum AddSubtraction
+    {
+      Add,
+      Subtract
+    }
+    }
   }
+    public class CalculationCommandFactory
+    {
+      public static void CreateCalculationCommand(int number)
+      {
+
+      }
+      public static void CreateCalculationCommand(double number)
+      {
+        
+      }
+       public static void CreateCalculationCommand(long number)
+      {
+        
+      }
+
+      public class CalculationCommand
+      {
+
+      }
+
+
+      public class FluentCalculator
+      {
+        public void calculate()
+        {
+
+        }
+
+        public void GetResult()
+        {
+
+        }
+      }
 }
