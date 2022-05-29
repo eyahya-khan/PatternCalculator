d6b6bbad5635fa1bb1616375b9c15d3d0f39d0dd
eyahya-khan <eyahyakhan@gmail.com>
2022-05-29 08:07:26 +0200

    Half Error have passed

--- Patterns.Calculator/Solution.cs
+++ Patterns.Calculator/Solution.cs
@@ -1,41 +1,143 @@
-﻿namespace Patterns
+﻿using System;
+
+namespace Patterns
 {
-       public enum AddSubtract
-        {
-          Add,
-          Subtract
-        }
-    public class CalculationCommandFactory
+    public enum OperationType
     {
-      public static void CreateCalculationCommand(int number)
-      {
-
-      }
-      public static void CreateCalculationCommand(double number)
-      {
-        
-      }
-       public static void CreateCalculationCommand(long number)
-      {
-        
-      }
+        Add,
+        Subtract
     }
-          public class CalculationCommand
-          {
 
-          }
-
-      public class FluentCalculator
-      {
-        public void calculate()
+    public static class CalculationCommandFactory
+    {
+        public static CalculationCommand CreateCalculationCommand(OperationType operation)
         {
+            switch (operation)
+            {
+                case OperationType.Add:
+                    return new AdditionCommand();
+                case OperationType.Subtract:
+                    return new SubtractionCommand();
+                default:
+                    throw new ApplicationException("Some Exception in code");
+            }
+        }
+            public static CalculationCommand CreateCalculationCommand(OperationType operation, int[] array)
+        {
+            switch (operation)
+            {
+                case OperationType.Add:
+                    return new AdditionCommand(array);
+                case OperationType.Subtract:
+                    return new SubtractionCommand(array);
+                default:
+                    throw new ApplicationException("Some Exception in code");
+            }          
+        }
+        
+        public static CalculationCommand CreateCalculationCommand(OperationType operation, int value)
+        {
+            switch (operation)
+            {
+                case OperationType.Add:
+                    return new AdditionCommand(value);
+                case OperationType.Subtract:
+                    return new SubtractionCommand(value);
+                default:
+                    throw new ApplicationException("Some Exception in code");
+            }
+        }
+    }
 
+    public interface CalculationCommand
+    { 
+        public OperationType OperationType { get; set; }
+        public int[] Arguments { get; set; }
+        public int GetResult();       
+    }
+
+    public class AdditionCommand : CalculationCommand
+    {
+        public int[] _arguments;     
+        public OperationType _operationtype;
+        public AdditionCommand()
+        {           
+        }
+        public AdditionCommand(int[] array)
+        {
+            this._arguments = array;
+        }
+        public AdditionCommand(int value)
+        {
+             this.Arguments = new int[] { value } ;
         }
 
-        public void GetResult()
+        public OperationType OperationType { get =>  this._operationtype; set => this.OperationType = value; }
+        public int[] Arguments { get => this._arguments; set => this._arguments = value; }
+
+        public int GetResult()
         {
+            int result = 0;
+            foreach (int item in Arguments)
+            {
+                result += item;
+            }
+            return result;
+        }        
+    }
+
+    public class SubtractionCommand : CalculationCommand
+    {
+        public int[] _arguments;       
+        public OperationType _operationtype;
+
+        public SubtractionCommand()
+        {
+           
+        }
+        public SubtractionCommand(int[] array)
+        {
+            this._arguments = array;
+        }
+
+        public SubtractionCommand(int value)
+        {
+            this.Arguments = new int[] { value };
+        }
+
+        public OperationType OperationType { get => this._operationtype; set => this.OperationType = value; }
+        public int[] Arguments { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+
+        public int GetResult()
+        {
+            int result = 0;
+            foreach (int item in Arguments)
+            {
+                result -= item;
+            }
+            return result;
+        }
+    }
+
+    public class FluentCalculator
+    {
+        private CalculationCommand command;
+        public FluentCalculator ()
+        {
+            this.command = new AdditionCommand();
+            this.command.Arguments = new int[] { };               
 
         }
-      }
+        public FluentCalculator Calculate(CalculationCommand cmd)
+        {
+            this.command = cmd;
+            return this;             
+        }
+        
+        public int GetResult()
+        {
+            return command.GetResult();
+        }
+    }
 
 }
--- Patterns.Tests/PatternCalculatorTests.cs
+++ Patterns.Tests/PatternCalculatorTests.cs
@@ -1,149 +1,153 @@
+using Xunit;
+using FluentAssertions;
+
+
+
 namespace Patterns.Tests
 {
   public class PatternCalculatorTests
   {
-    // [Fact]
-    // public void should_create_command_with_only_operation()
-    // {
-    //   // act
-    //   var cmd = CalculationCommandFactory.CreateCalculationCommand(OperationType.Add);
+    [Fact]
+    public void should_create_command_with_only_operation()
+    {
+      // act
+      var cmd = CalculationCommandFactory.CreateCalculationCommand(OperationType.Add);
 
-    //   // assert
-    //   cmd.Should().NotBe(null);
-    //   cmd.OperationType.Should().Be(OperationType.Add);
-    // }
+      // assert
+      cmd.Should().NotBe(null);
+      cmd.OperationType.Should().Be(OperationType.Add);
+    }
 
-    // [Fact]
-    // public void should_create_command_with_operation_and_single_argument()
-    // {
-    //   // act
-    //   var cmd = CalculationCommandFactory.CreateCalculationCommand(OperationType.Add, 12);
+    [Fact]
+    public void should_create_command_with_operation_and_single_argument()
+    {
+      // act
+      var cmd = CalculationCommandFactory.CreateCalculationCommand(OperationType.Add, 12);
 
-    //   // assert
-    //   cmd.Should().NotBe(null);
-    //   cmd.OperationType.Should().Be(OperationType.Add);
-    //   cmd.Arguments.Length.Should().Be(1);
-    // }
+      // assert
+      cmd.Should().NotBe(null);
+      cmd.OperationType.Should().Be(OperationType.Add);
+      cmd.Arguments.Length.Should().Be(1);
+    }
 
-    // [Fact]
-    // public void should_create_command_with_operation_and_array_of_arguments()
-    // {
-    //   // act
-    //   var cmd = CalculationCommandFactory.CreateCalculationCommand(OperationType.Add, new[] { 1, 2, 3, 4 });
+    [Fact]
+    public void should_create_command_with_operation_and_array_of_arguments()
+    {
+      // act
+      var cmd = CalculationCommandFactory.CreateCalculationCommand(OperationType.Add, new[] { 1, 2, 3, 4 });
 
-    //   // assert
-    //   cmd.Should().NotBe(null);
-    //   cmd.OperationType.Should().Be(OperationType.Add);
-    //   cmd.Arguments.Length.Should().Be(4);
-    // }
+      // assert
+      cmd.Should().NotBe(null);
+      cmd.OperationType.Should().Be(OperationType.Add);
+      cmd.Arguments.Length.Should().Be(4);
+    }
 
-    // [Fact]
-    // public void should_return_a_fluentCalculator_from_calculate()
-    // {
-    //   // arrange
-    //   var calc = new FluentCalculator();
-    //   var cmd = CalculationCommandFactory.CreateCalculationCommand(OperationType.Add);
+    [Fact]
+    public void should_return_a_fluentCalculator_from_calculate()
+    {
+      // arrange
+      var calc = new FluentCalculator();
+      var cmd = CalculationCommandFactory.CreateCalculationCommand(OperationType.Add);
 
-    //   // act
-    //   var next = calc.Calculate(cmd);
+      // act
+      var next = calc.Calculate(cmd);
 
-    //   // assert
-    //   next.Should().NotBe(null);
-    //   next.Should().BeOfType(typeof(FluentCalculator));
-    // }
+      // assert
+      next.Should().NotBe(null);
+      next.Should().BeOfType(typeof(FluentCalculator));
+    }
 
-    // [Fact]
-    // public void should_return_0_as_initial_result()
-    // {
-    //   // arrange
-    //   var calc = new FluentCalculator();
+    [Fact]
+    public void should_return_0_as_initial_result()
+    {
+      // arrange
+      var calc = new FluentCalculator();
 
-    //   // act
-    //   var result = calc.GetResult();
+      // act
+      var result = calc.GetResult();
 
-    //   // assert
-    //   result.Should().Be(0);
-    // }
+      // assert
+      result.Should().Be(0);
+    }
 
-    // [Fact]
-    // public void should_echo_argument_for_add_of_single_argument()
-    // {
-    //   // arrange
-    //   var calc = new FluentCalculator();
-    //   var cmd = CalculationCommandFactory.CreateCalculationCommand(OperationType.Add, 12);
+    [Fact]
+    public void should_echo_argument_for_add_of_single_argument()
+    {
+      // arrange
+      var calc = new FluentCalculator();
+      var cmd = CalculationCommandFactory.CreateCalculationCommand(OperationType.Add, 12);
 
-    //   // act
-    //   var result = calc.Calculate(cmd).GetResult();
+      // act
+      var result = calc.Calculate(cmd).GetResult();
 
-    //   // assert
-    //   result.Should().Be(12);
-    // }
+      // assert
+      result.Should().Be(12);
+    }
 
-    // [Fact]
-    // public void should_add_array_of_numbers()
-    // {
-    //   // arrange
-    //   var calc = new FluentCalculator();
-    //   var cmd = CalculationCommandFactory.CreateCalculationCommand(OperationType.Add, new[] { 1, 2, 3, 4, 5 });
+    [Fact]
+    public void should_add_array_of_numbers()
+    {
+      // arrange
+      var calc = new FluentCalculator();
+      var cmd = CalculationCommandFactory.CreateCalculationCommand(OperationType.Add, new[] { 1, 2, 3, 4, 5 });
 
-    //   // act
-    //   var result = calc.Calculate(cmd).GetResult();
+      // act
+      var result = calc.Calculate(cmd).GetResult();
 
-    //   // assert
-    //   result.Should().Be(15);
-    // }
+      // assert
+      result.Should().Be(15);
+    }
 
-    // [Fact]
-    // public void should_chain_add_commands()
-    // {
-    //   // arrange
-    //   var calc = new FluentCalculator();
-    //   var cmd1 = CalculationCommandFactory.CreateCalculationCommand(OperationType.Add, new[] { 1 });
-    //   var cmd2 = CalculationCommandFactory.CreateCalculationCommand(OperationType.Add, new[] { 1, 2 });
-    //   var cmd3 = CalculationCommandFactory.CreateCalculationCommand(OperationType.Add, new[] { 1, 2, 3 });
-    //   var cmd4 = CalculationCommandFactory.CreateCalculationCommand(OperationType.Add, new[] { 1, 2, 3, 4 });
+    [Fact]
+    public void should_chain_add_commands()
+    {
+      // arrange
+      var calc = new FluentCalculator();
+      var cmd1 = CalculationCommandFactory.CreateCalculationCommand(OperationType.Add, new[] { 1 });
+      var cmd2 = CalculationCommandFactory.CreateCalculationCommand(OperationType.Add, new[] { 1, 2 });
+      var cmd3 = CalculationCommandFactory.CreateCalculationCommand(OperationType.Add, new[] { 1, 2, 3 });
+      var cmd4 = CalculationCommandFactory.CreateCalculationCommand(OperationType.Add, new[] { 1, 2, 3, 4 });
 
-    //   // act
-    //   var result = calc
-    //     .Calculate(cmd1)
-    //     .Calculate(cmd2)
-    //     .Calculate(cmd3)
-    //     .Calculate(cmd4)
-    //     .GetResult();
+      // act
+      var result = calc
+        .Calculate(cmd1)
+        .Calculate(cmd2)
+        .Calculate(cmd3)
+        .Calculate(cmd4)
+        .GetResult();
 
-    //   // assert
-    //   result.Should().Be(20);
-    // }
+      // assert
+      result.Should().Be(20);
+    }
 
 
-    // [Fact]
-    // public void should_echo_argument_for_subtract_of_single_argument()
-    // {
-    //   // arrange
-    //   var calc = new FluentCalculator();
-    //   var cmd = CalculationCommandFactory.CreateCalculationCommand(OperationType.Subtract, 12);
+    [Fact]
+    public void should_echo_argument_for_subtract_of_single_argument()
+    {
+      // arrange
+      var calc = new FluentCalculator();
+      var cmd = CalculationCommandFactory.CreateCalculationCommand(OperationType.Subtract, 12);
 
-    //   // act
-    //   var result = calc.Calculate(cmd).GetResult();
+      // act
+      var result = calc.Calculate(cmd).GetResult();
+      
+      // assert
+      result.Should().Be(2);
+    }
 
-    //   // assert
-    //   result.Should().Be(-12);
-    // }
+    [Fact]
+    public void should_subtract_array_of_numbers()
+    {
+      // arrange
+      var calc = new FluentCalculator();
+      var cmd = CalculationCommandFactory.CreateCalculationCommand(OperationType.Subtract, new[] { 5, 4, 3, 2, 1 });
 
-    // [Fact]
-    // public void should_subtract_array_of_numbers()
-    // {
-    //   // arrange
-    //   var calc = new FluentCalculator();
-    //   var cmd = CalculationCommandFactory.CreateCalculationCommand(OperationType.Subtract, new[] { 5, 4, 3, 2, 1 });
+      // act
+      var result = calc.Calculate(cmd).GetResult();
 
-    //   // act
-    //   var result = calc.Calculate(cmd).GetResult();
-
-    //   // assert
-    //   result.Should().Be(-15);
-    // }
-
+      // assert
+      result.Should().Be(-15);
+    }
 
     // [Theory]
     // [InlineData(15, 15, 30)]
